4d4b4c9059479d5cf2c6a4fdaccb9471c9d89752
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-11-24 15:28:15 +0300

    Fix plylint issue

--- vega_admin/__init__.py
+++ vega_admin/__init__.py
@@ -3,4 +3,5 @@ Main init file for vega_admin
 """
 VERSION = (0, 0, 1)
 __version__ = '.'.join(str(v) for v in VERSION)
+# pylint: disable=invalid-name
 default_app_config = 'vega_admin.apps.VegaAdminConfig'  # noqa
