5fe44b93516ad558738a3c77154248de7651088f
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-12-22 23:51:12 +0300

    Make default actions configurable

--- vega_admin/settings.py
+++ vega_admin/settings.py
@@ -3,6 +3,18 @@ vega-admin settings module
 """
 from django.conf import settings
 
+# general
+VEGA_CREATE_ACTION = "create"
+VEGA_UPDATE_ACTION = "update"
+VEGA_LIST_ACTION = "list"
+VEGA_DELETE_ACTION = "delete"
+VEGA_DEFAULT_ACTIONS = [
+    VEGA_CREATE_ACTION,
+    VEGA_UPDATE_ACTION,
+    VEGA_LIST_ACTION,
+    VEGA_DELETE_ACTION,
+]
+
 # crispy forms
 VEGA_CRISPY_TEMPLATE_PACK = getattr(
     settings, "CRISPY_TEMPLATE_PACK", "bootstrap3")
