a15f61d72723cdeef063abf404ef981cf442f576
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-01-07 21:00:49 +0300

    Add custom action to example app

--- example/artists/views.py
+++ example/artists/views.py
@@ -1,5 +1,5 @@
 """Artist app views module"""
-from vega_admin.views import VegaCRUDView
+from vega_admin.views import VegaCRUDView, VegaListView
 
 from .models import Artist, Song
 
@@ -19,12 +19,19 @@ class SongCRUD(VegaCRUDView):
     CRUD view for songs
     """
 
+    class CustomListView(VegaListView):
+        """Custom list view"""
+        model = Artist
+
     model = Song
     protected_actions = None
     permissions_actions = None
     list_fields = ["name", "artist", ]
     read_fields = ["name", "artist", ]
     table_attrs = {"class": "table song-table"}
-    table_actions = ["create", "update", "delete", ]
+    table_actions = ["create", "artists", "update", "delete", ]
     create_fields = ["name", "artist", ]
     update_fields = ["name", ]
+    view_classes = {
+        "artists": CustomListView,
+    }
