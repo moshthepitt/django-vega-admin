7e219825388f87c8b40bb1e5844397e9d1c6818e
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-01-29 20:57:59 +0300

    Add stub tests for user forms

---
+++ tests/contrib/users/test_forms.py
@@ -0,0 +1,23 @@
+"""Test vega_admin.contrib.users.forms module"""
+
+from django.test import TestCase
+
+
+class TestForms(TestCase):
+    """
+    Test class for vega_admin.contrib.users.forms
+    """
+
+    def test_adduserform(self):
+        """
+        Test AddUserForm
+
+        """
+        self.fail()
+
+    def test_edituserform(self):
+        """
+        Test EditUserForm
+
+        """
+        self.fail()
