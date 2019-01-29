a2ef85a0d95c19624cdee484daa8e0e96fefb417
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-01-29 22:18:36 +0300

    Add test for ChangePassword view

---
+++ tests/contrib/users/test_views.py
@@ -0,0 +1,16 @@
+"""Test vega_admin.contrib.users.forms module"""
+
+from django.test import TestCase
+
+
+class TestViewss(TestCase):
+    """
+    Test class for vega_admin.contrib.users.views
+    """
+
+    def test_changepassword(self):
+        """
+        Test ChangePassword
+
+        """
+        self.fail()
