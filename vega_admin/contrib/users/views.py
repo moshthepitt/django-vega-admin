f93d3976c0f261f052480b1fc5d7980dc88ce46b
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-01-29 22:14:49 +0300

    Add change password view

--- vega_admin/contrib/users/views.py
+++ vega_admin/contrib/users/views.py
@@ -1,7 +1,24 @@
 """Views module for Vega Admin users app"""
-from vega_admin.views import VegaCRUDView
 from django.contrib.auth.models import User
-from vega_admin.contrib.users.forms import AddUserForm, EditUserForm
+from django.conf import settings
+from django.urls import reverse_lazy
+
+from vega_admin.contrib.users.forms import (AddUserForm, PasswordChangeForm,
+                                            EditUserForm)
+from vega_admin.views import VegaCRUDView, VegaUpdateView
+
+
+class ChangePassword(VegaUpdateView):  # pylint: disable=too-many-ancestors
+    """
+    Change Password view
+    """
+
+    form_class = PasswordChangeForm
+    model = User
+
+    def get_success_url(self):  # pylint: disable=no-self-use
+        """Get success_url"""
+        return reverse_lazy("auth.user-list")
 
 
 class UserCRUD(VegaCRUDView):
@@ -15,3 +32,12 @@ class UserCRUD(VegaCRUDView):
     update_form_class = EditUserForm
     protected_actions = None
     permissions_actions = None
+    view_classes = {
+        "change password": ChangePassword,
+    }
+    table_actions = [
+        settings.VEGA_READ_ACTION,
+        settings.VEGA_UPDATE_ACTION,
+        "change password",
+        settings.VEGA_DELETE_ACTION,
+    ]
