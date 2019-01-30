44d007fea013843701de9964cbd72f6ab1518fba
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-01-30 20:47:47 +0300

    Format code

--- vega_admin/contrib/users/views.py
+++ vega_admin/contrib/users/views.py
@@ -1,10 +1,10 @@
 """Views module for Vega Admin users app"""
-from django.contrib.auth.models import User, Group
 from django.conf import settings
+from django.contrib.auth.models import Group, User
 from django.urls import reverse_lazy
 
-from vega_admin.contrib.users.forms import (AddUserForm, PasswordChangeForm,
-                                            EditUserForm)
+from vega_admin.contrib.users.forms import (AddUserForm, EditUserForm,
+                                            PasswordChangeForm)
 from vega_admin.views import VegaCRUDView, VegaUpdateView
 
 
@@ -27,14 +27,12 @@ class UserCRUD(VegaCRUDView):
     """
 
     model = User
-    list_fields = ['id', 'username', 'email', 'first_name', 'last_name']
+    list_fields = ["id", "username", "email", "first_name", "last_name"]
     create_form_class = AddUserForm
     update_form_class = EditUserForm
     protected_actions = None
     permissions_actions = None
-    view_classes = {
-        "change password": ChangePassword,
-    }
+    view_classes = {"change password": ChangePassword}
     table_actions = [
         settings.VEGA_READ_ACTION,
         settings.VEGA_UPDATE_ACTION,
@@ -51,4 +49,4 @@ class GroupCRUD(VegaCRUDView):
     model = Group
     protected_actions = None
     permissions_actions = None
-    # list_fields = ['id', 'name']
+    list_fields = ['id', 'name']
