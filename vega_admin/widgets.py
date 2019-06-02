f0f90ad2a1a4f88144dc0871c06f8012e3f70e45
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-06-02 19:45:42 +0300

    Format widgets module

--- vega_admin/widgets.py
+++ vega_admin/widgets.py
@@ -1,17 +1,20 @@
 """Widgets module for django-vega-admin"""
-from django.forms import DateInput, TimeInput, DateTimeInput
+from django.forms import DateInput, DateTimeInput, TimeInput
 
 
 class VegaDateWidget(DateInput):
     """HTML5 Date input widget class"""
-    input_type = 'date'
+
+    input_type = "date"
 
 
 class VegaTimeWidget(TimeInput):
     """HTML5 Time input widget class"""
-    input_type = 'time'
+
+    input_type = "time"
 
 
 class VegaDateTimeWidget(DateTimeInput):
     """HTML5 Time input widget class"""
-    input_type = 'datetime-local'
+
+    input_type = "datetime-local"
