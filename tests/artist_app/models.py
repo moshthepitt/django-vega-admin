d116a121a5701580fbb32585c941208061e7c877
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-06-02 18:37:47 +0300

    Add fields to song model
    - Release date
    - Release time
    - Recording time

--- tests/artist_app/models.py
+++ tests/artist_app/models.py
@@ -8,12 +8,13 @@ class Artist(models.Model):
     """
     Artist Model class
     """
+
     name = models.CharField(max_length=100)
 
     class Meta:
-        ordering = ['name']
-        verbose_name = 'professional artist'
-        verbose_name_plural = 'professional artists'
+        ordering = ["name"]
+        verbose_name = "professional artist"
+        verbose_name_plural = "professional artists"
 
     def __str__(self):
         """Unicode representation of Song."""
@@ -22,14 +23,28 @@ class Artist(models.Model):
 
 class Song(models.Model):
     """Model definition for Song."""
+
+    SINGLE = "1"
+    COLLABO = "2"
+    SKIT = "3"
+
+    SONG_TYPES = ((SINGLE, "Single"), (COLLABO, "Collaboration"), (SKIT,
+                                                                   "Skit"))
+
     artist = models.ForeignKey(Artist, on_delete=models.PROTECT)
     name = models.CharField(max_length=100)
+    song_type = models.CharField(
+        "Type", max_length=1, choices=SONG_TYPES, default=SINGLE)
+    release_date = models.DateField("Release Date")
+    release_time = models.TimeField("Release Time")
+    recording_time = models.DateTimeField("Recording Time", auto_now_add=True)
 
     class Meta:
         """Meta definition for Song."""
-        verbose_name = 'Song'
-        verbose_name_plural = 'Songs'
-        ordering = ['name']
+
+        verbose_name = "Song"
+        verbose_name_plural = "Songs"
+        ordering = ["name"]
 
     def __str__(self):
         """Unicode representation of Song."""
