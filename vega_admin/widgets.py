1e310c6addeb5fed67fa508b1e7ad4f94fa997a4
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-06-02 19:27:17 +0300

    Add VegaTimeWidget

--- vega_admin/widgets.py
+++ vega_admin/widgets.py
@@ -1,7 +1,12 @@
 """Widgets module for django-vega-admin"""
-from django.forms import DateInput
+from django.forms import DateInput, TimeInput
 
 
 class VegaDateWidget(DateInput):
     """HTML5 Date input widget class"""
     input_type = 'date'
+
+
+class VegaTimeWidget(TimeInput):
+    """HTML5 Time input widget class"""
+    input_type = 'time'
