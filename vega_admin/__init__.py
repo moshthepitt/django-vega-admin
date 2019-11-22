cf378ecee8204c5eed057b06a9aa3fea35e2369d
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-11-22 16:25:55 +0300

    Bump to version 0.0.15

--- vega_admin/__init__.py
+++ vega_admin/__init__.py
@@ -1,7 +1,7 @@
 """
 Main init file for vega_admin
 """
-VERSION = (0, 0, 14)
+VERSION = (0, 0, 15)
 __version__ = ".".join(str(v) for v in VERSION)
 # pylint: disable=invalid-name
 default_app_config = "vega_admin.apps.VegaAdminConfig"  # noqa
