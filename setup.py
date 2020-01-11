9d5f8493a6520dfd6f51c5a1a793eb8e490938a2
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-01-11 15:14:24 +0300

    Drop django2.1 add django 3.0 support

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     url="https://github.com/moshthepitt/django-vega-admin",
     packages=find_packages(exclude=["docs", "tests"]),
     install_requires=[
-        "Django >=2.1.10",
+        "Django >=2.2",
         "django-crispy-forms",
         "django-braces",
         "django-filter",
@@ -49,8 +49,8 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Framework :: Django",
-        "Framework :: Django :: 2.1",
         "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
     ],
     include_package_data=True,
 )
